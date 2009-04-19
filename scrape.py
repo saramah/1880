e93f3479fbbea1979276ebe7f732ae711fd3b4a5
sarah <sarah.barbour@gmail.com>
2009-04-19 17:03:32 -0400

    most recent attempt

--- scrape.py
+++ scrape.py
@@ -1,4 +1,31 @@
+"""
+KEYS:
+    gsfn: firstname
+    gsln: lastname
+    f5: state
+    f4: county
+    f7: township
+    f42: relationship to head
+    f8: marital status
+    f15: race
+    f27: gender
+    f21: birthplace
+    rg_81004011__date: estimated birth year
+    rs_81004011__date: +/- for birth year
+    _8000C002: father's firstname
+    f28: father's birthplace
+    _80008002: mother's firstname
+    f16: mother's birthplace
+    _80018002: spouse name
+    f6: page number
+    f11: microfilm roll
+    f10: family history film
+    f22: enumeration district
+    f43: occupation
+    gskw: keywords
+"""
 import re
+import sys
 import time
 import mechanize
 from auth import *
@@ -10,13 +37,10 @@ def buildlist(path):
             build.append(line.strip())
     return build
 
-login_url = 'http://secure.ancestry.com/security/passwordlogin.aspx?'
+login_url = "http://secure.ancestry.com/security/passwordlogin.aspx?"
 
-state = "Massachusetts"
-county = "Suffolk"
-town = "Boston"
-enum_districts = range(787)[576:]
-relhead = "self"
+#enum_districts = range(787)[576:]
+enum_districts = range(787)[784:]
 occs = buildlist("occupations.txt")
 
 br = mechanize.Browser()
@@ -31,16 +55,23 @@ response = br.submit()
 search_url = response.read().split('\n')[0].split("\"")[1]
 br.open(search_url)
 
-census_search_url = 'http://content.ancestry.com/iexec/?htx=List&dbid=6742&enc=1&offerid=0%3a7858%3a0'
+census_search_url = "http://search.ancestry.com/cgi-bin/sse.dll?rank=1&gsfn=&gsln=&=&f5=MA&f4=Suffolk&f7=Boston&f42=Self&f8=&f15=&f27=&f21=&rg_81004011__date=&rs_81004011__date=0&_8000C002=&f28=&_80008002=&f16=&_80018002=&f6=&f11=&f10=&f22=%s&f43=%s&gskw=&prox=1&db=1880usfedcen&ti=0&ti.si=0&gl=&gss=IMAGE&gst=&so=3"
 
-br.open(census_search_url)
+occupation = "carpenter"
+#outfile = open("output", 'w')
+pattern = r'Matches <strong>.*<\/strong> of <strong>(\d|,)+<\/strong>'
+district = 777
 
-searchform_id = "s0searchbox"
-fname_id = "s0gsfn"
-lname_id = "s0gsln"
-state_id = "s0f5"
-county_id = "s0f4"
-town_id = "s0f7"
-relhead_id = "s0f42"
-enumdist_id = "s0f22"
-occ_id = "s0f43"
+#for district in enum_districts:
+to_open = census_search_url % (district, occupation)
+br.open(to_open)
+#    response = br.response().read()
+#    num_hits = 0
+#    if not re.search("Your Search returned no matches", response):
+#        num_hits = re.findall(pattern, response)[0].replace(",","")
+#    outfile.write("%s,%s,%s" % (district, occupation, num_hits))
+#    print response
+#    sys.exit()
+#    time.sleep(5)
+
+#outfile.close()
