a619e416871b8e221f7a849acbe3c0d1e96c96d2
sarah <sarah.barbour@gmail.com>
2009-07-12 01:21:34 -0700

    separated place, year

--- ancestry-place.js
+++ ancestry-place.js
@@ -25,30 +25,29 @@ var pl_length = places.length;
 var SEARCH_URL = ""
 
 if (REL_TO_HEAD == "Self"){
-  SEARCH_URL = "http://search.ancestrylibrary.com/cgi-bin/sse.dll?rank=0&gsfn=&gsln=&=&f5=MA&f4=Suffolk&f7=Boston&f42=Self&f8=&f15=&f27=&f21={0}&rg_81004011__date={1}&rs_81004011__date=5&_8000C002=&f28=&_80008002=&f16=&_80018002=&f6=&f11=&f10=&f22={2}&f43=&gskw=&prox=1&db=1880usfedcen&ti=5542&ti.si=0&gl=&gss=IMAGE&gst=&so=3";
+  SEARCH_URL = "http://search.ancestrylibrary.com/cgi-bin/sse.dll?rank=0&gsfn=&gsln=&=&f5=MA&f4=Suffolk&f7=Boston&f42=Self&f8=&f15=&f27=&f21={0}&rg_81004011__date=&rs_81004011__date=5&_8000C002=&f28=&_80008002=&f16=&_80018002=&f6=&f11=&f10=&f22={1}&f43=&gskw=&prox=1&db=1880usfedcen&ti=5542&ti.si=0&gl=&gss=IMAGE&gst=&so=3";
 } else {
-  SEARCH_URL = "http://search.ancestrylibrary.com/cgi-bin/sse.dll?rank=0&gsfn=&gsln=&=&f5=MA&f4=Suffolk&f7=Boston&f42=&f8=&f15=&f27=&f21={0}&rg_81004011__date={1}&rs_81004011__date=5&_8000C002=&f28=&_80008002=&f16=&_80018002=&f6=&f11=&f10=&f22={2}&f43=&gskw=&prox=1&db=1880usfedcen&ti=5542&ti.si=0&gl=&gss=IMAGE&gst=&so=3";
+  SEARCH_URL = "http://search.ancestrylibrary.com/cgi-bin/sse.dll?rank=0&gsfn=&gsln=&=&f5=MA&f4=Suffolk&f7=Boston&f42=&f8=&f15=&f27=&f21={0}&rg_81004011__date=&rs_81004011__date=5&_8000C002=&f28=&_80008002=&f16=&_80018002=&f6=&f11=&f10=&f22={1}&f43=&gskw=&prox=1&db=1880usfedcen&ti=5542&ti.si=0&gl=&gss=IMAGE&gst=&so=3";
 }
 var results = "";
 
 for(var index = 0, place = places[index]; index < pl_length; index++, place = places[index]){   
-  for(var year = 1805; year < 1880; year += 10){
     for(var district = LOWER_BOUND_DISTRICT; district <= UPPER_BOUND_DISTRICT; district++){
-      go(substitute(SEARCH_URL, [place, year, district]));
+      go(substitute(SEARCH_URL, [place, district]));
       var found = find(/\d+-\d+ of (\d+)/);
       if(found != "no matches"){
         var hits = /\d\-\d+ of (\d+)/.exec(found.text);
-        results += substitute("{0},{1},{2},{3}", [district, place, year, hits[1]]) + '\n';
-        output(substitute("{0},{1},{2},{3}", [district, place, year, hits[1]]))
+        results += substitute("{0},{1},{2}", [district, place, hits[1]]) + '\n';
+        output(substitute("{0},{1},{2}", [district, place, hits[1]]))
       } else {
-        results += substitute("{0},{1},{2},0", [district, place, year]) + '\n';
-        output(substitute("{0},{1},{2},0", [district, place, year]))
+        results += substitute("{0},{1},0", [district, place]) + '\n';
+        output(substitute("{0},{1},0", [district, place]))
       }
       sleep(5)
     }
-  }
 }
 write("output.csv", results);
+
 
 
 
